caed29985212342b12b0338e9bf2289f7510539d
xrayFailed <36075211+Ixve@users.noreply.github.com>
2025-07-22 16:37:56 +0200

    Update server.bat

--- server.bat
+++ server.bat
@@ -22,11 +22,11 @@ echo Running OpenSSL installer^.^.^.
 msiexec /i %USERPROFILE%\Downloads\OpenSSL.msi /qn
 
 echo Generating necessary certificates^.^.^.
-rmdir /s /q %USERPROFILE%\Documents\hozinumcert 2>nul
-mkdir %USERPROFILE%\Documents\hozinumcert 2>nul
+rmdir /s /q %USERPROFILE%\Documents\cert 2>nul
+mkdir %USERPROFILE%\Documents\cert 2>nul
 
 @REM TLS config (non-CA)
-> %USERPROFILE%\Documents\hozinumcert\tls.cnf (
+> %USERPROFILE%\Documents\cert\tls.cnf (
 echo [ req ]
 echo distinguished_name = dn
 echo prompt = no
@@ -43,21 +43,21 @@ echo DNS.1 = keyauth.win
 )
 
 @REM CA cert config
-> %USERPROFILE%\Documents\hozinumcert\ca.cnf (
+> %USERPROFILE%\Documents\cert\ca.cnf (
 echo [ v3_ca ]
 echo basicConstraints = CA:TRUE
 )
 
-%OPENSSL% genrsa -out %USERPROFILE%\Documents\hozinumcert\root.key 4096
-%OPENSSL% req -x509 -new -key %USERPROFILE%\Documents\hozinumcert\root.key -sha256 -days 825 -out %USERPROFILE%\Documents\hozinumcert\root.pem -subj "/CN=keyauth.win"
-certutil -addstore root %USERPROFILE%\Documents\hozinumcert\root.pem
+%OPENSSL% genrsa -out %USERPROFILE%\Documents\cert\root.key 4096
+%OPENSSL% req -x509 -new -key %USERPROFILE%\Documents\cert\root.key -sha256 -days 825 -out %USERPROFILE%\Documents\cert\root.pem -subj "/CN=keyauth.win"
+certutil -addstore root %USERPROFILE%\Documents\cert\root.pem
 
-%OPENSSL% genrsa -out %USERPROFILE%\Documents\hozinumcert\tls.key 2048
-%OPENSSL% req -new -key %USERPROFILE%\Documents\hozinumcert\tls.key -out %USERPROFILE%\Documents\hozinumcert\tls.csr -config %USERPROFILE%\Documents\hozinumcert\tls.cnf
-%OPENSSL% x509 -req -in %USERPROFILE%\Documents\hozinumcert\tls.csr -CA %USERPROFILE%\Documents\hozinumcert\root.pem -CAkey %USERPROFILE%\Documents\hozinumcert\root.key -CAcreateserial -out %USERPROFILE%\Documents\hozinumcert\tls.crt -days 365 -sha256 -extfile %USERPROFILE%\Documents\hozinumcert\tls.cnf -extensions v3
+%OPENSSL% genrsa -out %USERPROFILE%\Documents\cert\tls.key 2048
+%OPENSSL% req -new -key %USERPROFILE%\Documents\cert\tls.key -out %USERPROFILE%\Documents\cert\tls.csr -config %USERPROFILE%\Documents\cert\tls.cnf
+%OPENSSL% x509 -req -in %USERPROFILE%\Documents\cert\tls.csr -CA %USERPROFILE%\Documents\cert\root.pem -CAkey %USERPROFILE%\Documents\cert\root.key -CAcreateserial -out %USERPROFILE%\Documents\cert\tls.crt -days 365 -sha256 -extfile %USERPROFILE%\Documents\cert\tls.cnf -extensions v3
 
-%OPENSSL% genpkey -algorithm ed25519 -out %USERPROFILE%\Documents\hozinumcert\ed.key
-%OPENSSL% pkey -in %USERPROFILE%\Documents\hozinumcert\ed.key -pubout -out %USERPROFILE%\Documents\hozinumcert\ed.pub
+%OPENSSL% genpkey -algorithm ed25519 -out %USERPROFILE%\Documents\cert\ed.key
+%OPENSSL% pkey -in %USERPROFILE%\Documents\cert\ed.key -pubout -out %USERPROFILE%\Documents\cert\ed.pub
 
 echo Prerequisites
 %PYTHON% -m pip install pymem
